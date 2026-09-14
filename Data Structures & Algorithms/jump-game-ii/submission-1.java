8b6ac4a8c4fb4de78568ab6845aeb0469ea8a4b6
Ragini Gupta <gupta05ragini@gmail.com>
2026-09-14 11:51:02 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,39 @@
+// class Solution {
+//     public int jump(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         java.util.Arrays.fill(dp,-1);
+//         return solve(0, nums, dp);
+//     }
+
+//     private int solve(int ind, int[] nums, int[] dp){
+//         if(ind >= nums.length-1) return 0;
+//         if (dp[ind] != -1) return dp[ind];
+//         int mini = 10001;
+
+//         for (int i = 1; i <= nums[ind]; i++) {
+//             mini = Math.min(mini, 1 + solve(ind + i, nums, dp));
+//         }
+//         return dp[ind] = mini;
+//     }
+// }
+
+
+class Solution{
+    public int jump(int[] nums){
+        int n = nums.length;
+
+       int jump = 0;
+       int l = 0;
+       int r = 0;
+       while(r < n-1){
+        int farthest = 0;
+        for(int i=l; i<=r; i++){
+            farthest = Math.max(i+nums[i], farthest);
+        }
+        l=r+1;
+        jump = jump + 1;
+        r = farthest;
+       }
+       return jump; 
+    }
+}
