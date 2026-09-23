c5cc19128af87eed86f35899f81bc7632ce70d9f
Ragini Gupta <gupta05ragini@gmail.com>
2026-09-23 18:02:37 +0530

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int inc = 1;
+        int dec = 1;
+        int ans = 1;
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > nums[i-1]){
+                inc = inc + 1;
+                inc = 1;
+            }
+            else if(nums[i] < nums[i-1]){
+                dec = dec + 1;
+                inc = 1;
+            }
+            else{
+                inc = 1;
+                dec = 1;
+            }
+            ans = Math.max(ans, Math.max(inc, dec));
+        }
+        return ans;
+    }
+}
